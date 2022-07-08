7eddfbe6552b68b82fbbe6662bd007ffb312f7d9
MrIsCM <61559771+MrIsCM@users.noreply.github.com>
2022-07-08 20:39:08 +0200

    Sistema de colisiones para gas y roca. Simulacion con nr = 100 y ng = 500

--- planet.f95
+++ planet.f95
@@ -18,9 +18,9 @@ program planetesimales
 
 	
 	! Masa, posicion, velocidad, acelerock_acion, funcion aux w, 
-	double precision :: rock_m(1:rock_n), rock_x(1:rock_n), rock_y(1:rock_n), rock_vx(1:rock_n)
+	double precision :: rock_m(1:rock_n), rock_r(1:rock_n), rock_x(1:rock_n), rock_y(1:rock_n), rock_vx(1:rock_n)
 	double precision ::  rock_vy(1:rock_n), rock_ax(1:rock_n), rock_ay(1:rock_n), rock_wx(1:rock_n), rock_wy(1:rock_n)
-	double precision :: rock_Emec, rock_ESun
+	double precision :: rock_Emec, rock_ESun, rock_Qcol, rock_Et
 
 
 	!----------------------------------------------------------
@@ -28,11 +28,12 @@ program planetesimales
 	!----------------------------------------------------------
 
 	! Cantidad de planetesimales iniciales Tipo: Gas
-	integer, parameter :: gas_n = 450
+	integer, parameter :: gas_n = 500
 
 	! Masa, posicion, velocidad, aceleracion, funcion aux w, 
-	double precision :: gas_m(1:gas_n), gas_x(1:gas_n), gas_y(1:gas_n), gas_vx(1:gas_n), gas_vy(1:gas_n)
+	double precision :: gas_m(1:gas_n), gas_r(1:gas_n), gas_x(1:gas_n), gas_y(1:gas_n), gas_vx(1:gas_n), gas_vy(1:gas_n)
 	double precision ::  gas_ax(1:gas_n), gas_ay(1:gas_n), gas_wx(1:gas_n), gas_wy(1:gas_n)
+	double precision :: gas_Emec, gas_ESun, gas_Qcol, gas_Et 
 
 
 	!----------------------------------------------------------
@@ -53,7 +54,7 @@ program planetesimales
 
 	! Parametro iterativo
 	integer :: i, j
-	integer, parameter :: iter = 100000
+	integer, parameter :: iter = 2*1000000
 
 
 
@@ -69,7 +70,7 @@ program planetesimales
 	f_v = 3.357193253E-5					! f_r/f_t				! m/s
 
 	t=0
-	h=0.005
+	h=0.007
 
 	open(10, file='Datos/INFO_Simul.txt', status='unknown')
 		write(10,*) "Datos simulacion formacion S.Solar - Fortran95"
@@ -82,53 +83,102 @@ program planetesimales
 
 
 
-	call condiciones_iniciales(rock_m, rock_x, rock_y, rock_vx, rock_vy, rock_n)
-	call condiciones_iniciales(gas_m, gas_x, gas_y, gas_vx, gas_vy, gas_n)
+	call condiciones_iniciales_rock(rock_m, rock_r, rock_x, rock_y, rock_vx, rock_vy, rock_n)
+	call condiciones_iniciales_gas(gas_m, gas_r, gas_x, gas_y, gas_vx, gas_vy, gas_n)
 
 	call aceleraciones(rock_x, rock_y, rock_m, rock_ax, rock_ay, rock_n)
+	call aceleraciones(gas_x, gas_y, gas_m, gas_ax, gas_ay, gas_n)
+
 
 	! Energia 'destruida' al dejar de considerar las masas que se acercan demasiado a Sol
 	rock_ESun = 0 
+	rock_Qcol = 0
 
 	do i = 0, iter
 
-		! Algoritmo de Vervelet - Calculo de la posicion en t+h
+		! Deteccion de colisiones entre ROCOSOS
+		call colision_rock(rock_r, rock_x, rock_y, rock_vx, rock_vy, rock_m, rock_Qcol, rock_n)
+
+		! Deteccion de colisiones entre GASEOSO
+		call colision_gas(gas_r, gas_x, gas_y, gas_vx, gas_vy, gas_m, gas_Qcol, gas_n)
+
+		! 'Eliminacion' de masas por distancia al Sol  (ROCOSO)
+		call SunDist(rock_m, rock_x, rock_y, rock_vx, rock_vy, rock_ESun, rock_n)
+
+		! 'Eliminacion' de masas por distancia al Sol (GASEOSO)
+		call SunDist(gas_m, gas_x, gas_y, gas_vx, gas_vy, gas_ESun, gas_n)
+
+
+		! Algoritmo de Vervelet - Calculo de la posicion en t+h - Masas ROCOSAS
 		call vervelet_pos(rock_m, rock_x, rock_y, rock_vx, rock_vy, rock_ax, rock_ay, rock_n, h)
 		call w(rock_m, rock_vx, rock_vy, rock_ax, rock_ay, rock_wx, rock_wy, rock_n, h)
 		call vervelet_vel(rock_m, rock_vx, rock_vy, rock_ax, rock_ay, rock_wx, rock_wy, rock_n, h)
 		call aceleraciones(rock_x, rock_y, rock_m, rock_ax, rock_ay, rock_n)
 
-		if (mod(i,100)==0) then
+		! Algoritmo de Vervelet - Calculo de la posicion en t+h - Masas GASEOSAS
+		call vervelet_pos(gas_m, gas_x, gas_y, gas_vx, gas_vy, gas_ax, gas_ay, gas_n, h)
+		call w(gas_m, gas_vx, gas_vy, gas_ax, gas_ay, gas_wx, gas_wy, gas_n, h)
+		call vervelet_vel(gas_m, gas_vx, gas_vy, gas_ax, gas_ay, gas_wx, gas_wy, gas_n, h)
+		call aceleraciones(gas_x, gas_y, gas_m, gas_ax, gas_ay, gas_n)
+
+		if (mod(i,2000)==0) then
 			do j = 1, rock_n
-				if (rock_m(i) /= 0) then
-					write(1,*) t, rock_x(j), rock_y(j)
+				if (rock_m(j) /= 0) then
+					write(1,*) t, rock_x(j), rock_y(j), rock_m(j)
+				end if 
+			end do
+			do j = 1, gas_n
+				if (gas_m(j) /= 0) then
+					write(2,*) t, gas_x(j), gas_y(j), gas_m(j)
 				end if 
 			end do
 			write(1,*) 
 			write(1,*)
+			write(2,*) 
+			write(2,*)
 
 			call Ener_mec(rock_x, rock_y, rock_vx, rock_vy, rock_m, rock_Emec, rock_n)
-			rock_Emec = rock_Emec + rock_ESun
-			write(3,*) t, rock_Emec
-		end if 
+			call Ener_mec(gas_x, gas_y, gas_vx, gas_vy, gas_m, gas_Emec, gas_n)
+			rock_Et = rock_Emec + rock_ESun + rock_Qcol
+			gas_Et = gas_Emec + gas_ESun + gas_Qcol
 
-		! 'Eliminacion' de masas por proximidad al Sol
-		call SunDist(rock_m, rock_x, rock_y, rock_vx, rock_vy, rock_ESun, rock_n)
+			write(3,*) t, rock_Et + gas_Et, rock_Et, gas_Et
+		end if 
 
 		t = t+h
 	end do
 
-
 	close(1)
 	close(2)
 	close(3)
+
+	j=0
+	do i = 1, rock_n
+		if (rock_m(i)/=0) then
+			j = j+1
+		end if
+	end do
+
+	write(*,*) 'Quedan: ', j, ' masas ROCOSAS'
+
+	j=0
+	do i = 1, gas_n
+		if (gas_m(i)/=0) then
+			j = j+1
+		end if
+	end do
+
+	write(*,*) 'Quedan: ', j, ' masas GASEOSAS'
+
+
+	
 
 
 
 end program planetesimales
 
 
-subroutine condiciones_iniciales(m, x, y, vx, vy, n)
+subroutine condiciones_iniciales_rock(m, r, x, y, vx, vy, n)
 
 	!===========================================================================
 	!
@@ -156,7 +206,7 @@ subroutine condiciones_iniciales(m, x, y, vx, vy, n)
 	implicit none
 
 	integer, intent (in) :: n 
-	double precision,intent(inout) :: x(1:n), y(1:n), vx(1:n), vy(1:n), m(1:n)
+	double precision,intent(inout) :: x(1:n), y(1:n), vx(1:n), vy(1:n), m(1:n), r(1:n)
 	
 	integer :: i
 	double precision :: aux1(1:n), aux2(1:n), v(1:n), alpha 
@@ -173,8 +223,8 @@ subroutine condiciones_iniciales(m, x, y, vx, vy, n)
 	call random_number(aux2)
 	! call random_number(vx)
 
-	x = 40*x + 10 			! 10 < x < 50  Unidades Astronomicas
-	y = 80*y - 40			! -40 < y < 40  Unidades Astronomicas
+	x = 15*x + 0.01 		! 0.01 < x < 15.01  Unidades Astronomicas
+	y = 2*10*y - 10			! -10 < y < 10  	Unidades Astronomicas
 
 	do i = 1, n
 		v(i) = 1/(x(i)**2 + y(i)**2)**0.25 		! La raiz de la norma -- RAIZ((x^2+y^2)^1/2)
@@ -187,21 +237,90 @@ subroutine condiciones_iniciales(m, x, y, vx, vy, n)
 		end if 
 	end do
 
-	! do i = 1, n
-	! 	if (mod(i,4) == 0) then 
-	! 		vy(i) = -vy(i)		! Quiero que algunos giren en sentido opuesto
-	! 	end if 
-	! end do
-
-
+	
 	!------------------------
-	!		MASAS
+	!		MASA Y RADIO
 	!------------------------
 
-	m = 2.5E-10
+	m = 2.5E-8
+	r = 8E-6
+
+end subroutine condiciones_iniciales_rock
+
+subroutine condiciones_iniciales_gas(m, r, x, y, vx, vy, n)
+
+	!===========================================================================
+	!
+	!	Subrutina para adjudicar las condiciones iniciales.
+	!
+	!			+ El cojunto de posiciones i-esimas de cada vector corresponde
+	!			  con el conjunto de caracteristicas del cuerpo i-esimo
+	!
+	!			+ Primero se adjudica la posicion
+	!
+	!			+ La velocidad se calcula en funcion de la posicion
+	!			  y se le añade una magnitud aleatoria proporcional a v
+	!
+	!--------------------------------------------------------------------------
+	!	Parametros de entrada: 
+	!		- m: Vector de Masas
+	!		- x, y : Vectores de posiciones
+	!		- vx, vy: Vectores de las velocidades
+	!		- n : Numero de cuerpos de cada tipo (rocoso o gaseoso)
+	!--------------------------------------------------------------------------
+	!	Parametros de salida:
+	!		- x, y, vx, vy: Adjudica las posiciones y velocidades iniciales 
+	!===========================================================================
+
+	implicit none
+
+	integer, intent (in) :: n 
+	double precision,intent(inout) :: x(1:n), y(1:n), vx(1:n), vy(1:n), m(1:n), r(1:n)
+	
+	integer :: i
+	double precision :: aux1(1:n), aux2(1:n), v(1:n), alpha 
 
 
-end subroutine condiciones_iniciales
+	!-----------------------------------------
+	! 		POSICIONES Y VELOCIDADES
+	!-----------------------------------------
+
+	call random_seed()
+	call random_number(x)
+	call random_number(y)
+	call random_number(aux1)
+	call random_number(aux2)
+	! call random_number(vx)
+
+	x = -17*x - 3 		! -3 < x < -20  	Unidades Astronomicas
+	y = 2*15*y - 15		! -15 < y < 15  	Unidades Astronomicas
+
+	do i = 1, n
+		v(i) = 1/(x(i)**2 + y(i)**2)**0.25 		! La raiz de la norma -- RAIZ((x^2+y^2)^1/2)
+		alpha = atan(y(i)/x(i))
+		if ( alpha > 0 ) then 
+			vx(i) = -v(i) * sin(alpha)
+			vy(i) = v(i) * cos(alpha)*(0.2*aux1(i) + 0.9)
+		else
+			vx(i) = -v(i) * sin(alpha)
+			vy(i) = v(i) * cos(alpha)*(0.2*aux2(i) + 0.9)
+		end if 
+	end do
+
+	
+	!------------------------
+	!		MASA Y RADIO
+	!------------------------
+
+	! Refs: 
+	! 	Masa Jupiter: 1.898E27 [Kg] --> Mj/Ms = Mj' = 9.537688E-4 kg' 
+	! 	Radio Jupiter: 69.911E6 [m] -->Rj/c = Rj' 	= 4.673195E-4 m'
+	!	M/r^3 = 9345475.461 
+
+	m = 9.5E-6
+	r = 1.0054E-4
+
+end subroutine condiciones_iniciales_gas
 
 
 subroutine aceleraciones(x, y, m, ax, ay, n)
@@ -276,7 +395,6 @@ subroutine w(m, vx, vy, ax, ay, wx, wy, n, h)
 end subroutine w
 
 
-
 subroutine vervelet_vel(m, vx, vy, ax, ay, wx, wy, n, h)
 	implicit none
 
@@ -299,20 +417,22 @@ end subroutine vervelet_vel
 subroutine SunDist(m, x, y, vx, vy, ESun, n)
 	implicit none
 	integer, intent(in) :: n 
-	double precision, intent(in) :: x(1:n), y(1:n), vx(1:n), vy(1:n)
-	double precision, intent(inout) :: m(1:n), ESun 
+	double precision, intent(in) :: x(1:n), y(1:n) 
+	double precision, intent(inout) :: m(1:n), vx(1:n), vy(1:n), ESun 
 
 	integer :: i 
-	real, parameter :: R_Sol = 0.00465047**2 	! U.Astro 
+	real, parameter :: R_Sol2 = 2.162687122E-5 	! U.Astro 
 
 	do i = 1, n
 		! Si se acerca demasiado al Sol o se aleja demasiado
-		if (((x(i)**2 + y(i)**2) <=  R_Sol) .or. (x(i)**2 + y(i)**2) >= 80) then
+		if (((x(i)**2 + y(i)**2) <=  R_Sol2) .or. (x(i)**2 + y(i)**2) >= 80**2) then
 
 			! Calculo la E.Mec que tiene para tenerla en cuenta en la conserv. de la Energia
 			ESun = Esun + 0.5*m(i)*(vx(i)**2 + vy(i)**2) - m(i)/(x(i)**2+y(i)**2)**0.5
 
 			! 'Elimino' la masa. Comporbar siempre que m/=0
+			vx(i) = 0
+			vy(i) = 0
 			m(i) = 0
 
 		end if 
@@ -332,7 +452,7 @@ end subroutine SunDist
 !
 !	La velocidad de la masa resultante es la suma de las dos que colisionan.
 !
-subroutine colision(r, x, y, vx, vy, m, Qcol, n)
+subroutine colision_rock(r, x, y, vx, vy, m, Qcol, n)
 	
 	implicit none 
 	
@@ -345,13 +465,14 @@ subroutine colision(r, x, y, vx, vy, m, Qcol, n)
 	do i = 1, n
 		if (m(i) /= 0) then 
 			do j = i+1, n
-				if ( ((x(i)-x(j))**2 + (y(i)-y(j))**2) <= (1.2*(r(i)+r(j)))**2  ) then
+				if ( ((x(i)-x(j))**2 + (y(i)-y(j))**2) <= 6*(r(i)+r(j))  ) then
+					write(*,*) "Colision  producida"
 
-					! Energia disipada en la colision
+					! Energia disipada en la colision 
 						! Energia cinetica
 						Qcol = -0.5*m(i)*m(j)/(m(i)+m(j))*(sqrt(vx(i)**2+vy(i)**2)-sqrt(vx(j)**2+vy(j)**2))**2 
 
-						! Energia potencial
+						! + Energia potencial
 						Qcol = Qcol + m(j)*(1/sqrt(x(j)**2+y(j)**2) - 1/sqrt(x(i)**2+y(i)**2))
 
 					! Radio (variacion)
@@ -375,7 +496,57 @@ subroutine colision(r, x, y, vx, vy, m, Qcol, n)
 	end do
 
 	
-end subroutine colision
+end subroutine colision_rock
+
+
+!
+!	COLISIONES MASAS GAS
+!
+subroutine colision_gas(r, x, y, vx, vy, m, Qcol, n)
+	
+	implicit none 
+	
+	integer, intent(in) :: n
+	double precision, intent(in) :: x(1:n), y(1:n)
+	double precision, intent(inout) :: r(1:n), vx(1:n), vy(1:n), m(1:n), Qcol
+
+	integer :: i, j
+
+	do i = 1, n
+		if ((m(i) /= 0) .and. (x(i)**2+y(i)**2) >= 4**2) then 
+			do j = i+1, n
+				if ( ((x(i)-x(j))**2 + (y(i)-y(j))**2) <= 2*(r(i)+r(j))  ) then
+					write(*,*) "Colision  producida"
+
+					! Energia disipada en la colision 
+						! Energia cinetica
+						Qcol = -0.5*m(i)*m(j)/(m(i)+m(j))*(sqrt(vx(i)**2+vy(i)**2)-sqrt(vx(j)**2+vy(j)**2))**2 
+
+						! + Energia potencial
+						Qcol = Qcol + m(j)*(1/sqrt(x(j)**2+y(j)**2) - 1/sqrt(x(i)**2+y(i)**2))
+
+					! Radio (variacion)
+					r(i) = r(i)*((m(i)+m(j))/m(i))**(1.0/3.0)
+					r(j) = 0 
+
+					! Velocidades (variacion)
+					vx(i) = (m(i)*vx(i) + m(j)*vx(j))/(m(i)+m(j))
+					vy(i) = (m(i)*vy(i) + m(j)*vy(j))/(m(i)+m(j))
+
+					vx(j) = 0 
+					vy(j) = 0
+				
+					! Masa (variacion)
+					m(i) = m(i)+m(j)
+					m(j) = 0
+
+				end if
+			end do
+		end if
+	end do
+
+	
+end subroutine colision_gas
 
 
 !-----------------------------------------------------
@@ -399,10 +570,14 @@ subroutine Ener_mec(x, y, vx, vy, m, E_mec, n)
 	V = 0
 
 	do i = 1, n
-		T = T + 0.5*m(i)*(vx(i)**2 + vy(i)**2)
+		if (m(i)/=0) then
+			T = T + 0.5*m(i)*(vx(i)**2 + vy(i)**2)
+		end if 
 	end do
 	do i = 1, n
-		V = V - m(i)/(x(i)**2+y(i)**2)**0.5
+		if (m(i)/=0) then
+			V = V - m(i)/(x(i)**2+y(i)**2)**0.5
+		end if
 	end do
 
 	E_mec = T + V
--- plot.plt
+++ plot.plt
@@ -1,12 +1,19 @@
 set terminal gif animate delay 3
-set output "Gifs/R_SunProx.gif"  
+set output "Gifs/R100G500.gif"  
 
 rock = 'Datos/Rock.dat'
+gas = 'Datos/Gas.dat'
+
 
 set xrange[-60:60]
 set yrange[-60:60]
 
-do for [bb = 0:800]{
+stats rock nooutput
+stats gas nooutput
+
+do for [bb = 0:int(STATS_blocks)-2]{
 	set title sprintf('Frame:%03.0f',bb)
-	plot rock u 2:3 index bb 
+	plot 	rock u 2:3 index bb title 'Masas Rocosas',\
+			gas u 2:3 index bb pt 7 title 'Masas Gaseosa'
+
 }
